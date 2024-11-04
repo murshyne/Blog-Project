7cbcac65798e65c271269bac1eb39a27eced883f
Murshyne <murshyne@gmail.com>
2024-11-04 00:42:02 -0500

    Committed at: Mon, Nov  4, 2024 12:42:01 AM

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,16 @@
-const Home = () => {
-    return (
-      <div className="home">
-           <h2>Homepage</h2>
-      </div>
-    );
- }
- 
+function Home() {
+  const handleClick = () => {
+    console.log("Hello");
+    alert("Hello Modafuckers!!!");
+  };
+
+
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <button onClick={handleClick}>Click Me</button>
+    </div>
+  );
+}
+
 export default Home;
